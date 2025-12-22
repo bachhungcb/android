3e494cdfcbb6fe34122742de67451a9a13114289
bachhungcb <bachhungcb@gmail.com>
2025-12-22 15:16:26 +0700

    dmm

--- 18_11_2025/StudentManagement/app/src/main/java/com/example/mailclone/MainActivity.kt
+++ 18_11_2025/StudentManagement/app/src/main/java/com/example/mailclone/MainActivity.kt
@@ -1,10 +1,14 @@
-package com.example.mailclone
+package com.example.studentmanagement
+
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
